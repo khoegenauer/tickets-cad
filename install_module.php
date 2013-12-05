0cd78863729ce5d3f9016d3853129e62ed79c856
khoegenauer <hoegenauer.kevin@gmail.com>
2013-12-05 14:29:00 -0600

    Ran PHP-CS-Fixer (-unused_use, -braces, -visibility)

--- install_module.php
+++ install_module.php
@@ -12,10 +12,10 @@
 
 @session_start();
 
-require_once('./incs/functions.inc.php');
+require_once './incs/functions.inc.php';
 error_reporting(E_ALL);				// 2/3/09
 do_login(basename(__FILE__));	// session_start()
-$tickets_dir = getcwd();	
+$tickets_dir = getcwd();
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
 <HTML>
@@ -45,24 +45,25 @@ $tickets_dir = getcwd();
  * @see
  * @since
  */
-function prefix ($tbl) {		/* returns concatenated string */
-	global $mysql_prefix;
-	return  $mysql_prefix . $tbl;
-	}
+function prefix($tbl) {		/* returns concatenated string */
+    global $mysql_prefix;
+
+    return  $mysql_prefix . $tbl;
+    }
 
 $table_name = prefix("modules");	//	check to see if module table exists - if not create.
 $query = "CREATE TABLE IF NOT EXISTS `$table_name` (
   `id` int(4) NOT NULL AUTO_INCREMENT,
   `mod_name` varchar(48) NOT NULL,
   `mod_status` int(4) NOT NULL DEFAULT '0' COMMENT 'Set to 1 for module Active, 0 for module inactive',
-  `table` varchar(18) NOT NULL,		  
+  `table` varchar(18) NOT NULL,
   `affecting_files` varchar(128) NOT NULL COMMENT 'Files that use this module',
   `httpuser` varchar(48) DEFAULT NULL COMMENT 'For data sources behind protected areas',
-  `httppwd` varchar(48) DEFAULT NULL COMMENT 'For data sources behind protected areas',		  
+  `httppwd` varchar(48) DEFAULT NULL COMMENT 'For data sources behind protected areas',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;";
 $result = mysql_query($query) or do_error($query, 'mysql_query() failed', mysql_error(), __FILE__, __LINE__);
-if($result) {
+if ($result) {
 print gettext("Module Table Created");
 }
 
@@ -79,41 +80,42 @@ print gettext("Module Table Created");
  * @since
  */
 function get_mod_to_install() {
-	$to_install = array();
-	$current = array();
-	if(mod_table_exists("modules") == 1) {
-		$query = "SELECT * FROM $GLOBALS[mysql_prefix]modules`";
-		$result = mysql_query($query);
-		if($result) {
-			while($row = mysql_fetch_assoc($result)) {
-				$current[] = $row['mod_name'];
-				}
-			}
-		}		
-			
-	$entry = array();
-	$path = "./modules";
-	if ($handle = opendir($path)) {
-		while (false !== ($dirname = readdir($handle))) {
-			if ($dirname != "." && $dirname != "..") {
-				$entry[] = $dirname;
-				}
-			}
-		closedir($handle);
-		}
-	foreach ($entry as $val) {
-		if(!(in_array($val, $current))) {
-			$to_install[] = $val;
-			}
-		}
-	$ret_str = "<SELECT NAME='frm_module'>";
-	$i=1;
-	foreach($to_install as $val) {
-		$ret_str .= "<OPTION VALUE=" . $val . ">" . $val . "</OPTION>";
-		}
-	$ret_str .= "</SELECT>";
-	return $ret_str;
-	}
+    $to_install = array();
+    $current = array();
+    if (mod_table_exists("modules") == 1) {
+        $query = "SELECT * FROM $GLOBALS[mysql_prefix]modules`";
+        $result = mysql_query($query);
+        if ($result) {
+            while ($row = mysql_fetch_assoc($result)) {
+                $current[] = $row['mod_name'];
+                }
+            }
+        }
+
+    $entry = array();
+    $path = "./modules";
+    if ($handle = opendir($path)) {
+        while (false !== ($dirname = readdir($handle))) {
+            if ($dirname != "." && $dirname != "..") {
+                $entry[] = $dirname;
+                }
+            }
+        closedir($handle);
+        }
+    foreach ($entry as $val) {
+        if (!(in_array($val, $current))) {
+            $to_install[] = $val;
+            }
+        }
+    $ret_str = "<SELECT NAME='frm_module'>";
+    $i=1;
+    foreach ($to_install as $val) {
+        $ret_str .= "<OPTION VALUE=" . $val . ">" . $val . "</OPTION>";
+        }
+    $ret_str .= "</SELECT>";
+
+    return $ret_str;
+    }
 
 /**
  * module_tabs_exist
@@ -129,23 +131,23 @@ function get_mod_to_install() {
  * @since
  */
 function module_tabs_exist($name) {
-	$query 		= "SELECT COUNT(*) FROM `$GLOBALS[mysql_prefix]modules`";
-	$result 	= mysql_query($query);
-	$num_rows 	= @mysql_num_rows($result);
-	if($num_rows) {
-		$query_exists	= "SELECT * FROM `$GLOBALS[mysql_prefix]modules` WHERE `mod_name`=\"{$name}\"";
-		$result_exists	= mysql_query($query_exists) or do_error($query_exists, 'mysql_query() failed', mysql_error(), __FILE__, __LINE__);
-		$num_rows = mysql_num_rows($result_exists);
-		if($num_rows != 0) {
-			return 1;
-		} else {
-			return 0;
-		}	
-		} else {
-		return 0;
-		}
-	}
-	
+    $query 		= "SELECT COUNT(*) FROM `$GLOBALS[mysql_prefix]modules`";
+    $result 	= mysql_query($query);
+    $num_rows 	= @mysql_num_rows($result);
+    if ($num_rows) {
+        $query_exists	= "SELECT * FROM `$GLOBALS[mysql_prefix]modules` WHERE `mod_name`=\"{$name}\"";
+        $result_exists	= mysql_query($query_exists) or do_error($query_exists, 'mysql_query() failed', mysql_error(), __FILE__, __LINE__);
+        $num_rows = mysql_num_rows($result_exists);
+        if ($num_rows != 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+        } else {
+        return 0;
+        }
+    }
+
 /**
  * mod_table_exists
  * Insert description here
@@ -160,16 +162,16 @@ function module_tabs_exist($name) {
  * @since
  */
 function mod_table_exists($tablename) {			//check if mysql table exists, if it's a re-install
-	$query 		= "SELECT COUNT(*) FROM $tablename";
-	$result 	= mysql_query($query);
-	$num_rows 	= @mysql_num_rows($result);
-	if($num_rows) {
-		return 1;
-	} else {
-		return 0;
-	}
-	}
-	
+    $query 		= "SELECT COUNT(*) FROM $tablename";
+    $result 	= mysql_query($query);
+    $num_rows 	= @mysql_num_rows($result);
+    if ($num_rows) {
+        return 1;
+    } else {
+        return 0;
+    }
+    }
+
 /**
  * get_structure
  * Insert description here
@@ -184,29 +186,30 @@ function mod_table_exists($tablename) {			//check if mysql table exists, if it's
  * @since
  */
 function get_structure($structurefile) {
-	$xml_file = "./structure.xml";
-	$data="";
-	if (function_exists("curl_init")) {
-		$ch = curl_init();
-		$timeout = 5;
-		curl_setopt($ch, CURLOPT_URL, $xml_file);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-		$data = curl_exec($ch);
-		curl_close($ch);
-		}
-	else {				// not CURL
-		if ($fp = @fopen($xml_file, "r")) {
-			while (!feof($fp) && (strlen($data)<9000)) $data .= fgets($fp, 128);
-			fclose($fp);
-			}		
-		else {
-			print "-error " . __LINE__;		// @fopen fails
-			}
-	}
-	return $data;
-	}	// end function get_structure
-	
+    $xml_file = "./structure.xml";
+    $data="";
+    if (function_exists("curl_init")) {
+        $ch = curl_init();
+        $timeout = 5;
+        curl_setopt($ch, CURLOPT_URL, $xml_file);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        }
+    else {				// not CURL
+        if ($fp = @fopen($xml_file, "r")) {
+            while (!feof($fp) && (strlen($data)<9000)) $data .= fgets($fp, 128);
+            fclose($fp);
+            }
+        else {
+            print "-error " . __LINE__;		// @fopen fails
+            }
+    }
+
+    return $data;
+    }	// end function get_structure
+
 /**
  * write_path
  * Insert description here
@@ -222,84 +225,83 @@ function get_structure($structurefile) {
  * @since
  */
 function write_path($filepath, $tickets_dir) {
-	global $docRoot, $tickets_dir;
-	if (!$fp = fopen($tickets_dir.$filepath . '/path.inc.php', 'a')) {
-       	print '<LI><FONT CLASS="warn">' . gettext('Cannot open path.inc.php for writing') . '</FONT></LI>';
-	} else {
-		ftruncate($fp,0);			
-		fwrite($fp, "<?php\n");
-		fwrite($fp, "	/* " . gettext('generated by') . " '" . basename( __FILE__) . "' " . date('r') . " */\n");
-		fwrite($fp, '	$tickets_root 	= '."'$tickets_dir';\n");
-		fwrite($fp, '?>');
-	}
+    global $docRoot, $tickets_dir;
+    if (!$fp = fopen($tickets_dir.$filepath . '/path.inc.php', 'a')) {
+           print '<LI><FONT CLASS="warn">' . gettext('Cannot open path.inc.php for writing') . '</FONT></LI>';
+    } else {
+        ftruncate($fp,0);
+        fwrite($fp, "<?php\n");
+        fwrite($fp, "	/* " . gettext('generated by') . " '" . basename( __FILE__) . "' " . date('r') . " */\n");
+        fwrite($fp, '	$tickets_root 	= '."'$tickets_dir';\n");
+        fwrite($fp, '?>');
+    }
 
-	fclose($fp);
-	print '<LI> ' . gettext("Wrote Data Path URL") . '</LI><BR />';
-	}	
+    fclose($fp);
+    print '<LI> ' . gettext("Wrote Data Path URL") . '</LI><BR />';
+    }
 
 if (isset($_POST['submit'])) { // Handle the form.
-	print $_POST['frm_module'];
-	
+    print $_POST['frm_module'];
 
-	print "<DIV style='background-color:#CECECE; position: absolute; width: 60%; height: 60%; left: 20%; top: 10%; border:2px inset #FFF2BF; display: block; text-align: center'>";
-	
-	$structurefile = "./modules/" . $_POST['frm_module'] . "/structure.xml";
-	$xml = simplexml_load_file($structurefile);
-					
-	//	Read XML file for details of structure and other information.
-		
-	$modname = $xml->name;
-	$author = $xml->author;
-	$created = $xml->creationDate;
-	$version = $xml->version;
-	$description = $xml->description;
-	$directoryname = $xml->directories->directoryname;
-	$tablename = $xml->tables->tablename;		
-	$installfile = $xml->configuration->installfile;
-	$installfile = "./" . $installfile;
-	$modincsdir = $xml->directories->incsdirectoryname;
-	$modincsdir = "/" . $modincsdir;
 
-	$mod_entry_exists = module_tabs_exist($modname); // if a table entry aready exists for the module	
-	$mod_table_exists = mod_table_exists($tablename);
-	
-		if ((file_exists($directoryname)) && ($mod_entry_exists==1) && ($mod_table_exists==1)) { // checks fully functioning previous existance of module.
-		   echo gettext("The Module Directory and tables already exist. Please remove existing versions before installing a new one"); 
-		} else { 
+    print "<DIV style='background-color:#CECECE; position: absolute; width: 60%; height: 60%; left: 20%; top: 10%; border:2px inset #FFF2BF; display: block; text-align: center'>";
 
-			write_path($modincsdir, $tickets_dir);	
+    $structurefile = "./modules/" . $_POST['frm_module'] . "/structure.xml";
+    $xml = simplexml_load_file($structurefile);
 
-			// Print out Module details
+    //	Read XML file for details of structure and other information.
 
-			print gettext('Module Name') . ": " . $modname . "<BR />";
-			print gettext('Module Directory') . ": " . $directoryname . "<BR />";	
-			print gettext('Author') . ": " . $author . "<BR />";
-			print gettext('Created') . ": " . $created . "<BR />";
-			print gettext('Version') . ": " . $version . "<BR />";
-			print gettext('Description') . ": " . $description . "<BR />";
+    $modname = $xml->name;
+    $author = $xml->author;
+    $created = $xml->creationDate;
+    $version = $xml->version;
+    $description = $xml->description;
+    $directoryname = $xml->directories->directoryname;
+    $tablename = $xml->tables->tablename;
+    $installfile = $xml->configuration->installfile;
+    $installfile = "./" . $installfile;
+    $modincsdir = $xml->directories->incsdirectoryname;
+    $modincsdir = "/" . $modincsdir;
 
-			unlink($structurefile);
+    $mod_entry_exists = module_tabs_exist($modname); // if a table entry aready exists for the module
+    $mod_table_exists = mod_table_exists($tablename);
 
-			print "<A HREF=$installfile><< " . gettext('Configure Module') . " >></A>";
-			print "</DIV>";
-		}
+        if ((file_exists($directoryname)) && ($mod_entry_exists==1) && ($mod_table_exists==1)) { // checks fully functioning previous existance of module.
+           echo gettext("The Module Directory and tables already exist. Please remove existing versions before installing a new one");
+        } else {
+
+            write_path($modincsdir, $tickets_dir);
+
+            // Print out Module details
+
+            print gettext('Module Name') . ": " . $modname . "<BR />";
+            print gettext('Module Directory') . ": " . $directoryname . "<BR />";
+            print gettext('Author') . ": " . $author . "<BR />";
+            print gettext('Created') . ": " . $created . "<BR />";
+            print gettext('Version') . ": " . $version . "<BR />";
+            print gettext('Description') . ": " . $description . "<BR />";
+
+            unlink($structurefile);
+
+            print "<A HREF=$installfile><< " . gettext('Configure Module') . " >></A>";
+            print "</DIV>";
+        }
 } else { // If form not submitted print form.
 ?>
-	<DIV style='background-color:#CECECE; position: absolute; width: 60%; height: 60%; left: 20%; top: 10%; border:2px inset #FFF2BF; display: block'>	
-	<center><?php print gettext('TICKETS MODULES INSTALLATION.');?></center>
-	<DIV style='background-color:#CECECE; position: absolute; width: 40%; height: 20%; left: 5%; top: 10%; border:2px inset #FFF2BF; display: block'>
-	<TABLE BORDER="0" style='width: 100%;'>
-	<TH COLSPAN="2"><?php print gettext('Chose Module to Install');?><BR /></TH>
-	<FORM action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-	<fieldset>
-	<TR CLASS="even"><TD><?php print gettext('Module');?>:</TD><TD><?php print get_mod_to_install();?></TD></fieldset>
-	<TR CLASS="even"><TD COLSPAN="2" ALIGN="center"><input type="submit" name="submit" value="<?php print gettext('Submit');?>" /></TD></TR>
-	</FORM></TABLE>
-	</div>
-	<DIV style='background-color:#CECECE; position: absolute; width: 50%; height: 80%; right: 5%; top: 10%; border:2px inset #FFF2BF; display: block'>
-	<?php print gettext('HELP PANEL');?>
-	</DIV>
-	</DIV>
-	<?php
-	}
-?>
+    <DIV style='background-color:#CECECE; position: absolute; width: 60%; height: 60%; left: 20%; top: 10%; border:2px inset #FFF2BF; display: block'>
+    <center><?php print gettext('TICKETS MODULES INSTALLATION.');?></center>
+    <DIV style='background-color:#CECECE; position: absolute; width: 40%; height: 20%; left: 5%; top: 10%; border:2px inset #FFF2BF; display: block'>
+    <TABLE BORDER="0" style='width: 100%;'>
+    <TH COLSPAN="2"><?php print gettext('Chose Module to Install');?><BR /></TH>
+    <FORM action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <fieldset>
+    <TR CLASS="even"><TD><?php print gettext('Module');?>:</TD><TD><?php print get_mod_to_install();?></TD></fieldset>
+    <TR CLASS="even"><TD COLSPAN="2" ALIGN="center"><input type="submit" name="submit" value="<?php print gettext('Submit');?>" /></TD></TR>
+    </FORM></TABLE>
+    </div>
+    <DIV style='background-color:#CECECE; position: absolute; width: 50%; height: 80%; right: 5%; top: 10%; border:2px inset #FFF2BF; display: block'>
+    <?php print gettext('HELP PANEL');?>
+    </DIV>
+    </DIV>
+    <?php
+    }

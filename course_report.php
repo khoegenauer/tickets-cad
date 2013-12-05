a391ddb1c95da7bcbeb82c66610c33a83a88278b
khoegenauer <hoegenauer.kevin@gmail.com>
2013-12-05 13:36:49 -0600

    Ran PHP-CS-Fixer (-unused_use, -braces, -visibility)

--- course_report.php
+++ course_report.php
@@ -9,8 +9,8 @@
 12/15/11 - initial release
 12/19/11 'group' => 'group_name'
 */
-error_reporting(E_ALL);	
-require_once('./incs/functions.inc.php');
+error_reporting(E_ALL);
+require_once './incs/functions.inc.php';
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
 <HTML>
@@ -27,72 +27,71 @@ require_once('./incs/functions.inc.php');
 
 <?php
 //		dump($_POST);
-		$where = (intval($_POST['user_id']) > 0)? " WHERE `t`.`user_id` = {$_POST['user_id']}" : "";
-		
-		
-		$query = "SELECT * FROM `$GLOBALS[mysql_prefix]courses_taken` `t`
-			LEFT JOIN `$GLOBALS[mysql_prefix]user` `u` ON `u`.`id`=`t`.`user_id` 			
-			LEFT JOIN `$GLOBALS[mysql_prefix]courses` `c` ON `t`.`courses_id`=`c`.`id` 
-			{$where}
-			ORDER BY `u`.`name_l` ASC, `u`.`name_f` ASC, `c`.`group_name` ASC, `c`.`course` ASC";
-		$result = mysql_query($query) or do_error($query, 'mysql query failed', mysql_error(), __FILE__, __LINE__);
+        $where = (intval($_POST['user_id']) > 0)? " WHERE `t`.`user_id` = {$_POST['user_id']}" : "";
 
-		if (mysql_num_rows($result)==0) {		// no results - get selected userid 
-			$query 	= "SELECT * FROM  `$GLOBALS[mysql_prefix]user` WHERE `id` = {$_POST['user_id']} LIMIT 1";    			
-			$result	= mysql_query($query) or do_error($query,'mysql_query() failed',mysql_error(), basename( __FILE__), __LINE__);
-			$the_user_str = "";
-			if (mysql_num_rows($result)>0) {		// got a name?
-				$row = stripslashes_deep(mysql_fetch_assoc($result));
-				$the_user_str = " for {$row['user']}";
-				}
-		
-			echo "<BR /><BR /><center><H2>" . gettext('No class data') . "{$the_user_str}</H2><BR /><BR /><BR />";
-			}
-		else {
-			$evenodd = array ("even", "odd");	// CLASS names for alternating table row colors
-			$i = 0;
-			$cum_credits = 0;
-			$this_user= 0;
-			echo "<BR /><BR /><TABLE BORDER = 1 ALIGN=CENTER CELLPADDING = 2>
-				<TR CLASS = 'even'><TH COLSPAN=99>" . gettext('Classes Taken - <i><small>as of') . " " . date('M j, y', time()) . "</small></i></TH></TR>";
-			
-			while ($row = stripslashes_deep(mysql_fetch_assoc($result))) 	{
-				if ($row['user_id'] == $this_user ) {$cum_credits += $row['credits'];}
-				else								{$cum_credits = $row['credits']; $this_user = $row['user_id'];}
-				echo "<TR CLASS='{$evenodd[($i+1)%2]}' VALIGN='baseline'>\n\t\t
-					<TD>{$row['name_l']}, {$row['name_f']} {$row['name_mi']}</TD>
-					<TD>{$row['email']}</TD>
-					<TD>{$row['group_name']}</TD>
-					<TD>{$row['course']}</TD>
-					<TD>{$row['ident']}</TD>
-					<TD>{$row['credits']}</TD>
-					<TD>{$cum_credits}</TD>
-					<TD>{$row['date']}</TD>
-					";
-				$i++;
-				}
+        $query = "SELECT * FROM `$GLOBALS[mysql_prefix]courses_taken` `t`
+            LEFT JOIN `$GLOBALS[mysql_prefix]user` `u` ON `u`.`id`=`t`.`user_id`
+            LEFT JOIN `$GLOBALS[mysql_prefix]courses` `c` ON `t`.`courses_id`=`c`.`id`
+            {$where}
+            ORDER BY `u`.`name_l` ASC, `u`.`name_f` ASC, `c`.`group_name` ASC, `c`.`course` ASC";
+        $result = mysql_query($query) or do_error($query, 'mysql query failed', mysql_error(), __FILE__, __LINE__);
+
+        if (mysql_num_rows($result)==0) {		// no results - get selected userid
+            $query 	= "SELECT * FROM  `$GLOBALS[mysql_prefix]user` WHERE `id` = {$_POST['user_id']} LIMIT 1";
+            $result	= mysql_query($query) or do_error($query,'mysql_query() failed',mysql_error(), basename( __FILE__), __LINE__);
+            $the_user_str = "";
+            if (mysql_num_rows($result)>0) {		// got a name?
+                $row = stripslashes_deep(mysql_fetch_assoc($result));
+                $the_user_str = " for {$row['user']}";
+                }
+
+            echo "<BR /><BR /><center><H2>" . gettext('No class data') . "{$the_user_str}</H2><BR /><BR /><BR />";
+            }
+        else {
+            $evenodd = array ("even", "odd");	// CLASS names for alternating table row colors
+            $i = 0;
+            $cum_credits = 0;
+            $this_user= 0;
+            echo "<BR /><BR /><TABLE BORDER = 1 ALIGN=CENTER CELLPADDING = 2>
+                <TR CLASS = 'even'><TH COLSPAN=99>" . gettext('Classes Taken - <i><small>as of') . " " . date('M j, y', time()) . "</small></i></TH></TR>";
+
+            while ($row = stripslashes_deep(mysql_fetch_assoc($result))) {
+                if ($row['user_id'] == $this_user) {$cum_credits += $row['credits'];}
+                else								{$cum_credits = $row['credits']; $this_user = $row['user_id'];}
+                echo "<TR CLASS='{$evenodd[($i+1)%2]}' VALIGN='baseline'>\n\t\t
+                    <TD>{$row['name_l']}, {$row['name_f']} {$row['name_mi']}</TD>
+                    <TD>{$row['email']}</TD>
+                    <TD>{$row['group_name']}</TD>
+                    <TD>{$row['course']}</TD>
+                    <TD>{$row['ident']}</TD>
+                    <TD>{$row['credits']}</TD>
+                    <TD>{$cum_credits}</TD>
+                    <TD>{$row['date']}</TD>
+                    ";
+                $i++;
+                }
 ?>
-	</TABLE>
+    </TABLE>
 <?php
-	}			// end if/else
+    }			// end if/else
 ?>
 
-	<FORM NAME = 'course_form' METHOD = 'post' ACTION = '<?php echo basename(__FILE__)?>'>
-	<INPUT TYPE = 'hidden' NAME = 'user_id' VALUE = ''>
-	</FORM>
-	<SPAN STYLE='text-align: center; display: block;'><BR />
-	<?php print gettext('Another');?> &raquo; <SELECT NAME='frm_user_id' onChange = "document.course_form.user_id.value=this.options[this.selectedIndex].value; document.course_form.submit();">
-				<OPTION VALUE='' selected><?php print gettext('Select');?></OPTION>
-				<OPTION VALUE='0' ><?php print gettext('All users');?></OPTION>
+    <FORM NAME = 'course_form' METHOD = 'post' ACTION = '<?php echo basename(__FILE__)?>'>
+    <INPUT TYPE = 'hidden' NAME = 'user_id' VALUE = ''>
+    </FORM>
+    <SPAN STYLE='text-align: center; display: block;'><BR />
+    <?php print gettext('Another');?> &raquo; <SELECT NAME='frm_user_id' onChange = "document.course_form.user_id.value=this.options[this.selectedIndex].value; document.course_form.submit();">
+                <OPTION VALUE='' selected><?php print gettext('Select');?></OPTION>
+                <OPTION VALUE='0' ><?php print gettext('All users');?></OPTION>
 <?php
-	$query 	= "SELECT * FROM  `$GLOBALS[mysql_prefix]user` WHERE ((`name_l` IS NOT NULL) AND (LENGTH(`name_l`) > 0)) ORDER BY `name_l` ASC, `name_f` ASC";    			
-	$result	= mysql_query($query) or do_error($query,'mysql_query() failed',mysql_error(), basename( __FILE__), __LINE__);
-	while ($row = stripslashes_deep(mysql_fetch_assoc($result))) {
-		$the_opt = shorten("({$row['user']}) {$row['name_l']}, {$row['name_f']} {$row['name_mi']} ", 48);
-		echo "\t\t<OPTION VALUE='{$row['id']}'>{$the_opt}</OPTION>\n";
-		}				// end while()
+    $query 	= "SELECT * FROM  `$GLOBALS[mysql_prefix]user` WHERE ((`name_l` IS NOT NULL) AND (LENGTH(`name_l`) > 0)) ORDER BY `name_l` ASC, `name_f` ASC";
+    $result	= mysql_query($query) or do_error($query,'mysql_query() failed',mysql_error(), basename( __FILE__), __LINE__);
+    while ($row = stripslashes_deep(mysql_fetch_assoc($result))) {
+        $the_opt = shorten("({$row['user']}) {$row['name_l']}, {$row['name_f']} {$row['name_mi']} ", 48);
+        echo "\t\t<OPTION VALUE='{$row['id']}'>{$the_opt}</OPTION>\n";
+        }				// end while()
 ?>
-			</SELECT>
+            </SELECT>
 
 <INPUT TYPE = 'button' VALUE= "<?php print gettext('Finished');?>" onClick = "window.close();" STYLE = 'margin-left: 40px;'>
 </span>
--- delete_module.php
+++ delete_module.php
@@ -14,7 +14,7 @@
 require_once($_SESSION['fip']);
 error_reporting(E_ALL);				// 2/3/09
 do_login(basename(__FILE__));	// session_start()
-$tickets_dir = getcwd();	
+$tickets_dir = getcwd();
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
 <HTML>
@@ -43,23 +43,23 @@ $tickets_dir = getcwd();
  * @since
  */
 function module_tabs_exist($name) {
-	$query 		= "SELECT COUNT(*) FROM `$GLOBALS[mysql_prefix]modules`";
-	$result 	= mysql_query($query);
-	$num_rows 	= @mysql_num_rows($result);
-	if($num_rows) {
-		$query_exists	= "SELECT * FROM `$GLOBALS[mysql_prefix]modules` WHERE `mod_name`=\"{$name}\"";
-		$result_exists	= mysql_query($query_exists) or do_error($query_exists, 'mysql_query() failed', mysql_error(), __FILE__, __LINE__);
-		$num_rows = mysql_num_rows($result_exists);
-		if($num_rows != 0) {
-			return 1;
-		} else {
-			return 0;
-		}	
-		} else {
-		return 0;
-		}
-	}
-	
+    $query 		= "SELECT COUNT(*) FROM `$GLOBALS[mysql_prefix]modules`";
+    $result 	= mysql_query($query);
+    $num_rows 	= @mysql_num_rows($result);
+    if ($num_rows) {
+        $query_exists	= "SELECT * FROM `$GLOBALS[mysql_prefix]modules` WHERE `mod_name`=\"{$name}\"";
+        $result_exists	= mysql_query($query_exists) or do_error($query_exists, 'mysql_query() failed', mysql_error(), __FILE__, __LINE__);
+        $num_rows = mysql_num_rows($result_exists);
+        if ($num_rows != 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+        } else {
+        return 0;
+        }
+    }
+
 /**
  * mod_table_exists
  * Insert description here
@@ -74,49 +74,49 @@ function module_tabs_exist($name) {
  * @since
  */
 function mod_table_exists($tablename) {			//check if mysql table exists, if it's a re-install
-	$query 		= "SELECT COUNT(*) FROM $tablename";
-	$result 	= mysql_query($query);
-	$num_rows 	= @mysql_num_rows($result);
-	if($num_rows) {
-		return 1;
-	} else {
-		return 0;
-	}
-	}
-	
+    $query 		= "SELECT COUNT(*) FROM $tablename";
+    $result 	= mysql_query($query);
+    $num_rows 	= @mysql_num_rows($result);
+    if ($num_rows) {
+        return 1;
+    } else {
+        return 0;
+    }
+    }
+
 
 if (isset($_POST['module_choice'])) { // Handle the form.
 //	$query_exists	= "SELECT * FROM `$GLOBALS[mysql_prefix]modules` WHERE `mod_name`=\"{$name}\"";
 //	$result_exists	= mysql_query($query_exists) or do_error($query_exists, 'mysql_query() failed', mysql_error(), __FILE__, __LINE__);
 //	$num_rows = mysql_num_rows($result_exists);
 ?>
-	<DIV style='background-color:#CECECE; position: absolute; width: 60%; height: 60%; left: 20%; top: 10%; border:2px inset #FFF2BF; display: block; text-align: center'>
-	<TABLE BORDER="0">
-	<TR><TH class='heading'><?php print gettext('Module Deletion - Confirmation');?></TH></TR>
-	<TR><TD>&nbsp;</TD></TR>
-	<TR><TD>&nbsp;</TD></TR>
-	<FORM NAME="delete_2" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-	<TR><TD style='font-size: 14px; font-weight: bold; background-color: #AEAEAE; text-align: center;'><?php print gettext('Selected Module');?>: <?php print $_POST['module_choice'];?></TD></TR>
-	<TR><TD>&nbsp;</TD></TR>
-	<TR><TD>&nbsp;</TD></TR>
-	<TR><TD>&nbsp;</TD></TR>	
-	<TR><TD CLASS="td_label"><?php print gettext('Click Submit to confirm module deletion or Cancel to abort');?></TD></TR>	
-	<TR><TD>&nbsp;</TD></TR>
-	<TR><TD>&nbsp;</TD></TR>	
-	<INPUT TYPE='hidden' NAME='confirmation' VALUE='<?php print $_POST['module_choice'];?>'>
-	<INPUT TYPE='hidden' NAME='flag' VALUE='Confirmation Received'>	
-	<TR><TD ALIGN="center"><INPUT TYPE="button" VALUE="<?php print gettext('Cancel');?>" onClick="window.location.href='config.php';" />&nbsp;&nbsp;<input type="submit" name="submit" value="<?php print gettext('Submit');?>" /></TD></TR>
-	</FORM></TABLE>	
-	</DIV>
+    <DIV style='background-color:#CECECE; position: absolute; width: 60%; height: 60%; left: 20%; top: 10%; border:2px inset #FFF2BF; display: block; text-align: center'>
+    <TABLE BORDER="0">
+    <TR><TH class='heading'><?php print gettext('Module Deletion - Confirmation');?></TH></TR>
+    <TR><TD>&nbsp;</TD></TR>
+    <TR><TD>&nbsp;</TD></TR>
+    <FORM NAME="delete_2" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <TR><TD style='font-size: 14px; font-weight: bold; background-color: #AEAEAE; text-align: center;'><?php print gettext('Selected Module');?>: <?php print $_POST['module_choice'];?></TD></TR>
+    <TR><TD>&nbsp;</TD></TR>
+    <TR><TD>&nbsp;</TD></TR>
+    <TR><TD>&nbsp;</TD></TR>
+    <TR><TD CLASS="td_label"><?php print gettext('Click Submit to confirm module deletion or Cancel to abort');?></TD></TR>
+    <TR><TD>&nbsp;</TD></TR>
+    <TR><TD>&nbsp;</TD></TR>
+    <INPUT TYPE='hidden' NAME='confirmation' VALUE='<?php print $_POST['module_choice'];?>'>
+    <INPUT TYPE='hidden' NAME='flag' VALUE='Confirmation Received'>
+    <TR><TD ALIGN="center"><INPUT TYPE="button" VALUE="<?php print gettext('Cancel');?>" onClick="window.location.href='config.php';" />&nbsp;&nbsp;<input type="submit" name="submit" value="<?php print gettext('Submit');?>" /></TD></TR>
+    </FORM></TABLE>
+    </DIV>
 
-<?php	
+<?php
 } elseif (isset($_POST['confirmation'])) { // If form not submitted print form.
-	$mod_name = $_POST['confirmation'];
-	$query	= "SELECT * FROM `$GLOBALS[mysql_prefix]modules` WHERE `mod_name`= '$mod_name'";
-	$result	= mysql_query($query) or do_error($query, 'mysql_query() failed', mysql_error(), __FILE__, __LINE__);
-	$row = mysql_fetch_assoc($result);
-	$module_name = $row['mod_name'];
-	$table = $row['table'];	
+    $mod_name = $_POST['confirmation'];
+    $query	= "SELECT * FROM `$GLOBALS[mysql_prefix]modules` WHERE `mod_name`= '$mod_name'";
+    $result	= mysql_query($query) or do_error($query, 'mysql_query() failed', mysql_error(), __FILE__, __LINE__);
+    $row = mysql_fetch_assoc($result);
+    $module_name = $row['mod_name'];
+    $table = $row['table'];
 
 /**
  * rmdir_recurse
@@ -131,91 +131,91 @@ if (isset($_POST['module_choice'])) { // Handle the form.
  * @see
  * @since
  */
-	function rmdir_recurse($path) {  
-		$path = rtrim($path, '/').'/';  
-		$handle = opendir($path);  
-		while(false !== ($file = readdir($handle))) {  
-			if($file != '.' and $file != '..' ) {  
-				$fullpath = $path.$file;  
-				if(is_dir($fullpath)) rmdir_recurse($fullpath); else unlink($fullpath);  
-			}  
-		}  
-		closedir($handle);  
-		rmdir($path);
-		return TRUE;	
-	} 
-		
-?>
-	<DIV style='background-color:#CECECE; position: absolute; width: 60%; height: 60%; left: 20%; top: 10%; border:2px inset #FFF2BF; display: block; text-align: center'>
-	<BR /><BR /><BR /><BR /><?php	print $_POST['flag'];?><BR /><BR />
-	<?php print gettext('Deleting Tickets Module');?>........<?php print $_POST['confirmation'];?><BR /><BR />
-	<?php print gettext('Dropping Table');?>........<?php print $table;?>...........	
-<?php	
-	$query	= "DROP table `$GLOBALS[mysql_prefix]" . $table ."`";
-	$result	= mysql_query($query) or do_error($query, 'mysql_query() failed', mysql_error(), __FILE__, __LINE__);
-	if($result) {
-		print gettext('Success') . "<BR />";
-		} else {
-		print gettext('Failed') . "<BR />";
-		}
-?>	
+    function rmdir_recurse($path) {
+        $path = rtrim($path, '/').'/';
+        $handle = opendir($path);
+        while (false !== ($file = readdir($handle))) {
+            if ($file != '.' and $file != '..') {
+                $fullpath = $path.$file;
+                if(is_dir($fullpath)) rmdir_recurse($fullpath); else unlink($fullpath);
+            }
+        }
+        closedir($handle);
+        rmdir($path);
 
-	<?php print gettext('Removing Directory and files');?> /modules/<?php print $_POST['confirmation'];?>..........
-<?php
-	$directory = $tickets_dir . "/modules/" . $module_name;
-	$rem_dir = rmdir_recurse($directory);
-	if($rem_dir ==  true) {
-		print gettext('Success') . "<BR />";
-		} else {
-		print gettext('Failed') . "<BR />";
-		}	
+        return TRUE;
+    }
+
 ?>
-	<?php print gettext('Removing Entry from Modules Table.');?>.........
+    <DIV style='background-color:#CECECE; position: absolute; width: 60%; height: 60%; left: 20%; top: 10%; border:2px inset #FFF2BF; display: block; text-align: center'>
+    <BR /><BR /><BR /><BR /><?php	print $_POST['flag'];?><BR /><BR />
+    <?php print gettext('Deleting Tickets Module');?>........<?php print $_POST['confirmation'];?><BR /><BR />
+    <?php print gettext('Dropping Table');?>........<?php print $table;?>...........
 <?php
-	$query	= "DELETE FROM `$GLOBALS[mysql_prefix]modules` WHERE `mod_name`= '$mod_name'";
-	$result	= mysql_query($query) or do_error($query, 'mysql_query() failed', mysql_error(), __FILE__, __LINE__);
-	if($result) {
-		print gettext('Success') . "<BR />";
-		} else {
-		print gettext('Failed') . "<BR />";
-		}
-?>		
-	<BR /><BR /><?php print gettext('Module Removed Successfuly');?><BR /><BR />
-	<A HREF="config.php"><< <?php print gettext('Return to Configuration Page');?> >></A>
-	</DIV>
+    $query	= "DROP table `$GLOBALS[mysql_prefix]" . $table ."`";
+    $result	= mysql_query($query) or do_error($query, 'mysql_query() failed', mysql_error(), __FILE__, __LINE__);
+    if ($result) {
+        print gettext('Success') . "<BR />";
+        } else {
+        print gettext('Failed') . "<BR />";
+        }
+?>
+
+    <?php print gettext('Removing Directory and files');?> /modules/<?php print $_POST['confirmation'];?>..........
+<?php
+    $directory = $tickets_dir . "/modules/" . $module_name;
+    $rem_dir = rmdir_recurse($directory);
+    if ($rem_dir ==  true) {
+        print gettext('Success') . "<BR />";
+        } else {
+        print gettext('Failed') . "<BR />";
+        }
+?>
+    <?php print gettext('Removing Entry from Modules Table.');?>.........
+<?php
+    $query	= "DELETE FROM `$GLOBALS[mysql_prefix]modules` WHERE `mod_name`= '$mod_name'";
+    $result	= mysql_query($query) or do_error($query, 'mysql_query() failed', mysql_error(), __FILE__, __LINE__);
+    if ($result) {
+        print gettext('Success') . "<BR />";
+        } else {
+        print gettext('Failed') . "<BR />";
+        }
+?>
+    <BR /><BR /><?php print gettext('Module Removed Successfuly');?><BR /><BR />
+    <A HREF="config.php"><< <?php print gettext('Return to Configuration Page');?> >></A>
+    </DIV>
 <?php
 
 } else {
 
-	$query	= "SELECT * FROM `$GLOBALS[mysql_prefix]modules`";
-	$result	= mysql_query($query) or do_error($query, 'mysql_query() failed', mysql_error(), __FILE__, __LINE__);
-	$num = mysql_num_rows($result);
+    $query	= "SELECT * FROM `$GLOBALS[mysql_prefix]modules`";
+    $result	= mysql_query($query) or do_error($query, 'mysql_query() failed', mysql_error(), __FILE__, __LINE__);
+    $num = mysql_num_rows($result);
 
-	
-	$choice = "<SELECT name='module_choice' >";
-	$choice .= "<OPTION style='color:#FFFF00; background-color:#CC0000;' selected>" . gettext('Select Module to Delete') . "</OPTION>";
-	while ($row = mysql_fetch_assoc($result)) {
-		$module_name = $row['mod_name'];
-		$table = $row['table'];
-		$choice .= "<OPTION VALUE='$module_name'>$module_name</OPTION>";
-		}
-	$choice .= "</SELECT>";
+
+    $choice = "<SELECT name='module_choice' >";
+    $choice .= "<OPTION style='color:#FFFF00; background-color:#CC0000;' selected>" . gettext('Select Module to Delete') . "</OPTION>";
+    while ($row = mysql_fetch_assoc($result)) {
+        $module_name = $row['mod_name'];
+        $table = $row['table'];
+        $choice .= "<OPTION VALUE='$module_name'>$module_name</OPTION>";
+        }
+    $choice .= "</SELECT>";
 
 ?>
-	<DIV style='background-color:#CECECE; position: absolute; width: 60%; height: 60%; left: 20%; top: 10%; border:2px inset #FFF2BF; display: block'>	
-	<center><?php print gettext('TICKETS MODULES INSTALLATION.');?></center>
-	<DIV style='background-color:#CECECE; position: absolute; width: 40%; height: 20%; left: 5%; top: 10%; border:2px inset #FFF2BF; display: block'>
-	<TABLE BORDER="0">
-	<TH COLSPAN="2"><?php print gettext('Delete a Tickets Module');?><BR /></TH>
-	<FORM NAME="delete_1" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-	<TR CLASS="even"><TD CLASS="td_label"><?php print gettext('Module');?>: </TD><TD><?php print $choice;?></TD>
-	<TR CLASS="even"><TD COLSPAN="2" ALIGN="center"><input type="submit" name="submit" value="<?php print gettext('Submit');?>" /></TD></TR>
-	</FORM></TABLE>
-	</div>
-	<DIV style='background-color:#CECECE; position: absolute; width: 50%; height: 80%; right: 5%; top: 10%; border:2px inset #FFF2BF; display: block'>
-	<?php print gettext('HELP PANEL');?>
-	</DIV>
-	</DIV>
-	<?php
-	}
-?>
+    <DIV style='background-color:#CECECE; position: absolute; width: 60%; height: 60%; left: 20%; top: 10%; border:2px inset #FFF2BF; display: block'>
+    <center><?php print gettext('TICKETS MODULES INSTALLATION.');?></center>
+    <DIV style='background-color:#CECECE; position: absolute; width: 40%; height: 20%; left: 5%; top: 10%; border:2px inset #FFF2BF; display: block'>
+    <TABLE BORDER="0">
+    <TH COLSPAN="2"><?php print gettext('Delete a Tickets Module');?><BR /></TH>
+    <FORM NAME="delete_1" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <TR CLASS="even"><TD CLASS="td_label"><?php print gettext('Module');?>: </TD><TD><?php print $choice;?></TD>
+    <TR CLASS="even"><TD COLSPAN="2" ALIGN="center"><input type="submit" name="submit" value="<?php print gettext('Submit');?>" /></TD></TR>
+    </FORM></TABLE>
+    </div>
+    <DIV style='background-color:#CECECE; position: absolute; width: 50%; height: 80%; right: 5%; top: 10%; border:2px inset #FFF2BF; display: block'>
+    <?php print gettext('HELP PANEL');?>
+    </DIV>
+    </DIV>
+    <?php
+    }

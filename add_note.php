ed68c987ee0d889625668c43ccf790ada05ac8eb
khoegenauer <hoegenauer.kevin@gmail.com>
2013-12-05 12:22:38 -0600

    Ran PHP-CS-Fixer (-unused_use, -braces, -visibility)

--- add_note.php
+++ add_note.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 
+ *
  * @package add_note.php
  * @author John Doe <john.doe@example.com>
  * @since 2009-08-10
@@ -19,14 +19,14 @@
 error_reporting(E_ALL);
 
 @session_start();
-require_once('./incs/functions.inc.php');		//7/28/10
-if($istest) {
+require_once './incs/functions.inc.php';		//7/28/10
+if ($istest) {
 //	dump(basename(__FILE__));
-	print "GET<br />\n";
-	dump($_GET);
-	print "POST<br />\n";
-	dump($_POST);
-	}
+    print "GET<br />\n";
+    dump($_GET);
+    print "POST<br />\n";
+    dump($_POST);
+    }
 $disposition = get_text("Disposition");				// 12/1/10
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
@@ -42,29 +42,31 @@ $disposition = get_text("Disposition");				// 12/1/10
 <LINK REL=StyleSheet HREF="stylesheet.php?version=<?php print time();?>" TYPE="text/css">	<!-- 3/15/11 -->
 <SCRIPT>
 /**
- * 
+ *
  * @returns {unresolved}
- */  
-	String.prototype.trim = function () {				// 3/16/10
-		return this.replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1");
-		};
+ */
+    String.prototype.trim = function () {				// 3/16/10
+
+        return this.replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1");
+        };
 /**
- * 
+ *
  * @returns {Boolean}
- */    
-function validate () {
-	if(document.frm_note.frm_text.value.trim().length==0) {
-		alert("<?php print gettext('Enter text - or Cancel');?>"); 
-		return false;
-		} 
-	else {
-		document.frm_note.submit();	
-		}
-	}
+ */
+function validate() {
+    if (document.frm_note.frm_text.value.trim().length==0) {
+        alert("<?php print gettext('Enter text - or Cancel');?>");
+
+        return false;
+        }
+    else {
+        document.frm_note.submit();
+        }
+    }
 </SCRIPT>
 </HEAD>
 <?php
-if (empty($_POST)) { 
+if (empty($_POST)) {
 ?>
 <BODY onLoad = "document.frm_note.frm_text.focus();">
 <CENTER>
@@ -74,28 +76,28 @@ if (empty($_POST)) {
 <BR />
 <SCRIPT>
 /**
- * 
+ *
  * @param {type} inval
  * @returns {undefined}
- */  
-	function set_signal(inval) {
-		var temp_ary = inval.split("|", 2);		// inserted separator
-		document.frm_note.frm_text.value+=" " + temp_ary[1] + ' ';		
-		document.frm_note.frm_text.focus();		
-		}		// end function set_signal()
+ */
+    function set_signal(inval) {
+        var temp_ary = inval.split("|", 2);		// inserted separator
+        document.frm_note.frm_text.value+=" " + temp_ary[1] + ' ';
+        document.frm_note.frm_text.focus();
+        }		// end function set_signal()
 </SCRIPT>
 
-Signal &raquo; 
+Signal &raquo;
 <SELECT NAME='signals' onChange = 'set_signal(this.options[this.selectedIndex].text); this.options[0].selected=true;'>	<!--  11/17/10 -->
 <OPTION VALUE=0 SELECTED><?php print gettext('Select');?></OPTION>
 <?php
-				$query = "SELECT * FROM `$GLOBALS[mysql_prefix]codes` ORDER BY `sort` ASC, `code` ASC";
-				$result = mysql_query($query) or do_error($query, 'mysql query failed', mysql_error(),basename( __FILE__), __LINE__);
-				while ($row_sig = stripslashes_deep(mysql_fetch_assoc($result))) {
-					print "\t<OPTION VALUE='{$row_sig['code']}'>{$row_sig['code']}|{$row_sig['text']}</OPTION>\n";		// pipe separator
-					}
+                $query = "SELECT * FROM `$GLOBALS[mysql_prefix]codes` ORDER BY `sort` ASC, `code` ASC";
+                $result = mysql_query($query) or do_error($query, 'mysql query failed', mysql_error(),basename( __FILE__), __LINE__);
+                while ($row_sig = stripslashes_deep(mysql_fetch_assoc($result))) {
+                    print "\t<OPTION VALUE='{$row_sig['code']}'>{$row_sig['code']}|{$row_sig['text']}</OPTION>\n";		// pipe separator
+                    }
 ?>
-			</SELECT><BR /><BR />
+            </SELECT><BR /><BR />
 
 <B><?php print gettext('Apply to');?></B>&nbsp;:&nbsp;&nbsp;
 <?php print gettext('Description');?> &raquo; <INPUT TYPE = 'radio' NAME='frm_add_to' value='0' CHECKED />&nbsp;&nbsp;&nbsp;&nbsp;
@@ -108,31 +110,31 @@ Signal &raquo;
 <INPUT TYPE = 'hidden' NAME = 'frm_ticket_id' VALUE='<?php print $_GET['ticket_id']; ?>' />
 </FORM>
 <?php
-		}		// end if (empty($_POST))
-	else {
-		$field_name = array('description', 'comments');
-		$query = "SELECT * FROM `$GLOBALS[mysql_prefix]ticket` WHERE `id` = {$_POST['frm_ticket_id']} LIMIT 1";
-		$result = mysql_query($query) or do_error($query, 'mysql query failed', mysql_error(), basename( __FILE__), __LINE__);
-		$row = stripslashes_deep(mysql_fetch_assoc($result));
-		$now = (time() - (get_variable('delta_mins')*60)); 
-		$format = get_variable('date_format');
-		$the_date = date($format, $now);
-		$the_in_str = ($_POST['frm_add_to']=="0")? $row['description'] : $row['comments'] ;
-		@session_start();		
-		$the_text = "{$the_in_str} [{$_SESSION['user']}:{$the_date}]" . strip_tags(trim($_POST['frm_text'])) . "\n";		// 1/7/2013
-	
-		$query = "UPDATE `$GLOBALS[mysql_prefix]ticket` SET `{$field_name[$_POST['frm_add_to']]}`= " . quote_smart($the_text) . " WHERE `id` = " . quote_smart($_POST['frm_ticket_id'])  ." LIMIT 1";
-		$result = mysql_query($query) or do_error($query, 'mysql query failed', mysql_error(), __FILE__, __LINE__);
+        }		// end if (empty($_POST))
+    else {
+        $field_name = array('description', 'comments');
+        $query = "SELECT * FROM `$GLOBALS[mysql_prefix]ticket` WHERE `id` = {$_POST['frm_ticket_id']} LIMIT 1";
+        $result = mysql_query($query) or do_error($query, 'mysql query failed', mysql_error(), basename( __FILE__), __LINE__);
+        $row = stripslashes_deep(mysql_fetch_assoc($result));
+        $now = (time() - (get_variable('delta_mins')*60));
+        $format = get_variable('date_format');
+        $the_date = date($format, $now);
+        $the_in_str = ($_POST['frm_add_to']=="0")? $row['description'] : $row['comments'] ;
+        @session_start();
+        $the_text = "{$the_in_str} [{$_SESSION['user']}:{$the_date}]" . strip_tags(trim($_POST['frm_text'])) . "\n";		// 1/7/2013
+
+        $query = "UPDATE `$GLOBALS[mysql_prefix]ticket` SET `{$field_name[$_POST['frm_add_to']]}`= " . quote_smart($the_text) . " WHERE `id` = " . quote_smart($_POST['frm_ticket_id'])  ." LIMIT 1";
+        $result = mysql_query($query) or do_error($query, 'mysql query failed', mysql_error(), __FILE__, __LINE__);
 //		dump ($query);
-		$quick = (intval(get_variable('quick'))==1);				// 12/16/09
-		if ($quick) {
+        $quick = (intval(get_variable('quick'))==1);				// 12/16/09
+        if ($quick) {
 ?>
-	<BODY onLoad = "opener.location.reload(true); opener.parent.frames['upper'].show_msg ('Note added!'); window.close();">
-	</BODY></HTML>
-	
+    <BODY onLoad = "opener.location.reload(true); opener.parent.frames['upper'].show_msg ('Note added!'); window.close();">
+    </BODY></HTML>
+
 <?php
-			}				// end if ($quick)
-		else {
+            }				// end if ($quick)
+        else {
 ?>
 <BODY onLoad = "opener.location.reload(true);"><CENTER>
 <BR /><BR />
@@ -142,7 +144,6 @@ Signal &raquo;
 </BODY>
 </HTML>
 <?php
-		unset($result);
-		}		// end if/else (quick)
-	}		// end if/else (empty())
-?>
+        unset($result);
+        }		// end if/else (quick)
+    }		// end if/else (empty())

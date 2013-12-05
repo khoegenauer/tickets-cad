b91d02a94599dd7390c2f6ea9d5dc1ada9dbc32d
khoegenauer <hoegenauer.kevin@gmail.com>
2013-12-05 13:40:18 -0600

    Ran PHP-CS-Fixer (-unused_use, -braces, -visibility)

--- do_aprs.php
+++ do_aprs.php
@@ -1,11 +1,9 @@
 <?php
 /**
- *  
+ *
  */
-error_reporting(E_ALL);	
-require_once('./incs/functions.inc.php');
+error_reporting(E_ALL);
+require_once './incs/functions.inc.php';
 do_aprs();
 
 print gettext('finished') . " " . gettext('finished') . " " . gettext('finished') . " " . gettext('finished') . " " . gettext('finished') . " " . gettext('finished');
-
-?>

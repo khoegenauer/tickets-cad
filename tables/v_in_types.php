0260180ce1b1a292f8909ba8a492de15b3ab4511
khoegenauer <hoegenauer.kevin@gmail.com>
2013-12-05 16:33:28 -0600

    Ran PHP-CS-Fixer (-unused_use, -braces, -visibility)

--- tables/v_in_types.php
+++ tables/v_in_types.php
@@ -7,42 +7,43 @@
  */
 ?>
 <FORM NAME="v" METHOD="post" ACTION="<?php print $_SERVER['PHP_SELF']; ?>" >
-	<INPUT TYPE="hidden" NAME="tablename" 	VALUE="<?php print $tablename;?>"/>
-	<INPUT TYPE="hidden" NAME="indexname" 	VALUE="id"/>
-	<INPUT TYPE="hidden" NAME="sortby" 		VALUE="id"/>
-	<INPUT TYPE="hidden" NAME="sortdir"		VALUE=0 />
+    <INPUT TYPE="hidden" NAME="tablename" 	VALUE="<?php print $tablename;?>"/>
+    <INPUT TYPE="hidden" NAME="indexname" 	VALUE="id"/>
+    <INPUT TYPE="hidden" NAME="sortby" 		VALUE="id"/>
+    <INPUT TYPE="hidden" NAME="sortdir"		VALUE=0 />
 <?php
 /**
- * 
+ *
  * @param type $id
  * @return string
  */
 function get_mailgroup_name($id) {	//	8/28/13
-	if($id == 0) {
-		return "";
-		}
-	$query = "SELECT * FROM `$GLOBALS[mysql_prefix]mailgroup` WHERE `id` = " . $id;
-	$result = mysql_query($query) or do_error($query, 'mysql query failed', mysql_error(),basename( __FILE__), __LINE__);
-	$row = stripslashes_deep(mysql_fetch_assoc($result));
-	$the_ret = $row['name'];
-	return $the_ret;
-	}
+    if ($id == 0) {
+        return "";
+        }
+    $query = "SELECT * FROM `$GLOBALS[mysql_prefix]mailgroup` WHERE `id` = " . $id;
+    $result = mysql_query($query) or do_error($query, 'mysql query failed', mysql_error(),basename( __FILE__), __LINE__);
+    $row = stripslashes_deep(mysql_fetch_assoc($result));
+    $the_ret = $row['name'];
+
+    return $the_ret;
+    }
 ?>
 <TABLE BORDER="0" ALIGN="center" ><TR CLASS="even" VALIGN="top"><TD COLSPAN="2" ALIGN="CENTER"><FONT SIZE="+1"><?php print gettext("Table 'Incident types' - View Entry");?></FONT></TD></TR><TR><TD>&nbsp;</TD></TR>
-	<TR CLASS='odd' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('ID');?>:</TD>			<TD><?php print $row['id'];?></TD></TR>
-	<TR CLASS='even' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Type');?>:</TD>		<TD><?php print $row['type'];?></TD></TR>
-	<TR CLASS='odd' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Description');?>:</TD>	<TD><?php print $row['description'];?></TD></TR>
-	<TR CLASS='even' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Protocol');?>:</TD>	<TD><?php print $row['protocol'];?></TD></TR>
+    <TR CLASS='odd' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('ID');?>:</TD>			<TD><?php print $row['id'];?></TD></TR>
+    <TR CLASS='even' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Type');?>:</TD>		<TD><?php print $row['type'];?></TD></TR>
+    <TR CLASS='odd' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Description');?>:</TD>	<TD><?php print $row['description'];?></TD></TR>
+    <TR CLASS='even' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Protocol');?>:</TD>	<TD><?php print $row['protocol'];?></TD></TR>
 
-	<TR CLASS='odd' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Severity');?>:</TD>	<TD><?php print get_severity($row['set_severity']); ?> </TD></TR>
-	<TR CLASS='even' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Group');?>:</TD>		<TD><?php print $row['group'];?></TD></TR>
-	<TR CLASS='odd' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Sort');?>:</TD>		<TD><?php print $row['sort'];?></TD></TR>
-	<TR CLASS='even' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Radius');?>:</TD>		<TD><?php print $row['radius'];?></TD></TR>
-	<TR CLASS='odd' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Color');?>:</TD>		<TD><?php print $row['color'];?></TD></TR>
-	<TR CLASS='even' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Opacity');?>:</TD><TD><?php print $row['opacity'];?></TD></TR>
-	<TR CLASS='odd' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Notify Mailgroup');?>:</TD><TD><?php print get_mailgroup_name($row['notify_mailgroup']);?></TD></TR>
-	<TR CLASS='even' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Notify Email');?>:</TD><TD><?php print $row['notify_email'];?></TD></TR>
-	<TR><TD COLSPAN="2" ALIGN="center">
-	<BR />
-<?php	
+    <TR CLASS='odd' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Severity');?>:</TD>	<TD><?php print get_severity($row['set_severity']); ?> </TD></TR>
+    <TR CLASS='even' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Group');?>:</TD>		<TD><?php print $row['group'];?></TD></TR>
+    <TR CLASS='odd' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Sort');?>:</TD>		<TD><?php print $row['sort'];?></TD></TR>
+    <TR CLASS='even' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Radius');?>:</TD>		<TD><?php print $row['radius'];?></TD></TR>
+    <TR CLASS='odd' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Color');?>:</TD>		<TD><?php print $row['color'];?></TD></TR>
+    <TR CLASS='even' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Opacity');?>:</TD><TD><?php print $row['opacity'];?></TD></TR>
+    <TR CLASS='odd' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Notify Mailgroup');?>:</TD><TD><?php print get_mailgroup_name($row['notify_mailgroup']);?></TD></TR>
+    <TR CLASS='even' VALIGN="top"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Notify Email');?>:</TD><TD><?php print $row['notify_email'];?></TD></TR>
+    <TR><TD COLSPAN="2" ALIGN="center">
+    <BR />
+<?php
 //	7/11/10 corrections to imclude $row values
--- tables/v_personnel.php
+++ tables/v_personnel.php
@@ -8,69 +8,68 @@
 ?>
 <SCRIPT>
 /**
- * 
+ *
  * @param {type} myform
  * @returns {undefined}
  */
 function validate_form(myform) {	// reject empty form elements
-	myform.frm_date_of_birth.value = myform.frm_year_date_of_birth.value + "-" + myform.frm_month_date_of_birth.value + "-" + myform.frm_day_date_of_birth.value + " 00:00:00";
-	myform.submit();		
-	}				// end function 
+    myform.frm_date_of_birth.value = myform.frm_year_date_of_birth.value + "-" + myform.frm_month_date_of_birth.value + "-" + myform.frm_day_date_of_birth.value + " 00:00:00";
+    myform.submit();
+    }				// end function
 </SCRIPT>
-		<FORM NAME="v" METHOD="post" ACTION="<?php print $_SERVER['PHP_SELF']; ?>">
-		<INPUT TYPE="hidden" NAME="func" 		VALUE="pu" />
-		<INPUT TYPE="hidden" NAME="tablename" 	VALUE="<?php print $tablename;?>" />
-		<INPUT TYPE="hidden" NAME="indexname" 	VALUE="id" />
-		<INPUT TYPE="hidden" NAME="sortby" 		VALUE="id" />
-		<INPUT TYPE="hidden" NAME="sortdir"		VALUE=0 />
-		<INPUT TYPE="hidden" NAME="frm__by" 	VALUE="<?php print $_SESSION['user_id']; ?>" />
-		<INPUT TYPE="hidden" NAME="frm__from" 	VALUE="<?php print $_SERVER['REMOTE_ADDR']; ?>" />
-		<INPUT TYPE="hidden" NAME="frm__on" 	VALUE="<?php print mysql_format_date(time() - (get_variable('delta_mins')*60));?>" />
-		<INPUT TYPE="hidden" NAME="id" 			VALUE="<?php print $row['id'];?>" />
-	
-		<TABLE BORDER="0" ALIGN="center">
-		<TR CLASS="even" VALIGN="top"><TD COLSPAN="2" ALIGN="CENTER"><FONT SIZE="+1"><?php print gettext("Table 'Conditions' - View Entry");?></FONT></TD></TR>
-		<TR><TD>&nbsp;</TD></TR>
-		<TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Surname');?>:</TD>
-			<TD><?php print $row['surname'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Fornames');?>:</TD>
-			<TD><?php print $row['forenames'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Address');?>:</TD>
-			<TD><?php print $row['address'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('State');?>:</TD>
-			<TD><?php print $row['state'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Date of Birth');?>:</TD>
-			<TD><?php print format_date_2(strtotime($row['date_of_birth']));?></TD></TR>
-		<TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Gender');?>:</TD>
-			<TD><?php print $row['gender'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Identifier');?>:</TD>
-			<TD><?php print $row['person_identifier'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Email');?>:</TD>
-			<TD><?php print $row['email'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Home phone');?>:</TD>
-			<TD><?php print $row['homephone'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Work phone');?>:</TD>
-			<TD><?php print $row['workphone'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Cellphone');?>:</TD>
-			<TD><?php print $row['cellphone'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Next of Kin Name');?>:</TD>
-			<TD><?php print $row['next_of_kin_name'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Next of Kin Address');?>:</TD>
-			<TD><?php print $row['next_of_kin_address'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Next of Kin Home phone');?>:</TD>
-			<TD><?php print $row['next_of_kin_homephone'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Next of Kin Work phone');?>:</TD>
-			<TD><?php print $row['next_of_kin_workphone'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Next of Kin Cellphone');?>:</TD>
-			<TD><?php print $row['next_of_kin_cellphone'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('AR Callsign');?>:</TD>
-			<TD><?php print $row['amateur_radio_callsign'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Notes');?>:</TD>
-			<TD><?php print $row['person_notes'];?></TD></TR>
-		<TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Capabilities');?>:</TD>
-			<TD><?php print $row['person_capabilities'];?></TD></TR>
-		<TR><TD COLSPAN="99" ALIGN="center">
-		<BR />
+        <FORM NAME="v" METHOD="post" ACTION="<?php print $_SERVER['PHP_SELF']; ?>">
+        <INPUT TYPE="hidden" NAME="func" 		VALUE="pu" />
+        <INPUT TYPE="hidden" NAME="tablename" 	VALUE="<?php print $tablename;?>" />
+        <INPUT TYPE="hidden" NAME="indexname" 	VALUE="id" />
+        <INPUT TYPE="hidden" NAME="sortby" 		VALUE="id" />
+        <INPUT TYPE="hidden" NAME="sortdir"		VALUE=0 />
+        <INPUT TYPE="hidden" NAME="frm__by" 	VALUE="<?php print $_SESSION['user_id']; ?>" />
+        <INPUT TYPE="hidden" NAME="frm__from" 	VALUE="<?php print $_SERVER['REMOTE_ADDR']; ?>" />
+        <INPUT TYPE="hidden" NAME="frm__on" 	VALUE="<?php print mysql_format_date(time() - (get_variable('delta_mins')*60));?>" />
+        <INPUT TYPE="hidden" NAME="id" 			VALUE="<?php print $row['id'];?>" />
 
+        <TABLE BORDER="0" ALIGN="center">
+        <TR CLASS="even" VALIGN="top"><TD COLSPAN="2" ALIGN="CENTER"><FONT SIZE="+1"><?php print gettext("Table 'Conditions' - View Entry");?></FONT></TD></TR>
+        <TR><TD>&nbsp;</TD></TR>
+        <TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Surname');?>:</TD>
+            <TD><?php print $row['surname'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Fornames');?>:</TD>
+            <TD><?php print $row['forenames'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Address');?>:</TD>
+            <TD><?php print $row['address'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('State');?>:</TD>
+            <TD><?php print $row['state'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Date of Birth');?>:</TD>
+            <TD><?php print format_date_2(strtotime($row['date_of_birth']));?></TD></TR>
+        <TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Gender');?>:</TD>
+            <TD><?php print $row['gender'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Identifier');?>:</TD>
+            <TD><?php print $row['person_identifier'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Email');?>:</TD>
+            <TD><?php print $row['email'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Home phone');?>:</TD>
+            <TD><?php print $row['homephone'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Work phone');?>:</TD>
+            <TD><?php print $row['workphone'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Cellphone');?>:</TD>
+            <TD><?php print $row['cellphone'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Next of Kin Name');?>:</TD>
+            <TD><?php print $row['next_of_kin_name'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Next of Kin Address');?>:</TD>
+            <TD><?php print $row['next_of_kin_address'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Next of Kin Home phone');?>:</TD>
+            <TD><?php print $row['next_of_kin_homephone'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Next of Kin Work phone');?>:</TD>
+            <TD><?php print $row['next_of_kin_workphone'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Next of Kin Cellphone');?>:</TD>
+            <TD><?php print $row['next_of_kin_cellphone'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('AR Callsign');?>:</TD>
+            <TD><?php print $row['amateur_radio_callsign'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="even"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Notes');?>:</TD>
+            <TD><?php print $row['person_notes'];?></TD></TR>
+        <TR VALIGN="baseline" CLASS="odd"><TD CLASS="td_label" ALIGN="right"><?php print gettext('Capabilities');?>:</TD>
+            <TD><?php print $row['person_capabilities'];?></TD></TR>
+        <TR><TD COLSPAN="99" ALIGN="center">
+        <BR />
 
 <?php
